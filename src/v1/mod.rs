9aa76742a90062fd5c0fb875f25d2ac68f7fc084
Marc Noirot <marc.noirot@gmail.com>
2023-07-29 01:59:43 +0200

    Add GPP string lazy types

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -1,6 +1,7 @@
 use crate::core::{DataReader, DecodeExt};
 use crate::sections::id::GPP_HEADER;
 use crate::sections::{decode_section, Section, SectionDecodeError};
+use std::collections::HashMap;
 use std::io;
 use std::str::FromStr;
 use thiserror::Error;
@@ -34,30 +35,7 @@ impl FromStr for GPPModel {
     type Err = GPPDecodeError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut sections_iter = s.split('~');
-
-        let header_str = sections_iter.next().ok_or(GPPDecodeError::NoHeaderFound)?;
-        let header = header_str.decode_base64_url()?;
-        let mut reader = DataReader::new(&header);
-
-        let header_type = reader.read_fixed_integer::<u8>(6)?;
-        if header_type != GPP_HEADER {
-            return Err(GPPDecodeError::InvalidHeaderType { found: header_type });
-        }
-
-        let gpp_version = reader.read_fixed_integer::<u8>(6)?;
-        if gpp_version != GPP_VERSION {
-            return Err(GPPDecodeError::InvalidGPPVersion { found: gpp_version });
-        }
-
-        let section_ids = reader.read_fibonacci_range()?;
-        let sections = sections_iter.collect::<Vec<_>>();
-        if sections.len() != section_ids.len() {
-            return Err(GPPDecodeError::IdSectionMismatch {
-                ids: section_ids.len(),
-                sections: sections.len(),
-            });
-        }
+        let (section_ids, sections) = extract_gpp_sections_from_str(s)?;
 
         let sections = section_ids
             .iter()
@@ -72,13 +50,148 @@ impl FromStr for GPPModel {
     }
 }
 
+pub trait SectionMapper {
+    fn section(&self, id: u8) -> Option<&str>;
+    fn section_ids(&self) -> &[u8];
+
+    fn decode_section(&self, id: u8) -> Option<Result<Section, SectionDecodeError>> {
+        let s = self.section(id)?;
+        Some(decode_section(id, s))
+    }
+
+    fn decode_all_sections(&self) -> Result<Vec<Section>, SectionDecodeError> {
+        self.section_ids()
+            .iter()
+            .filter_map(|id| self.decode_section(*id))
+            .collect::<Result<Vec<_>, _>>()
+    }
+}
+
+pub trait AsGPPStr {
+    fn as_gpp_str(&self) -> Result<GPPStr, GPPDecodeError>;
+}
+
+impl AsGPPStr for &str {
+    fn as_gpp_str(&self) -> Result<GPPStr, GPPDecodeError> {
+        GPPStr::extract_from_str(self)
+    }
+}
+
+pub struct GPPStr<'a> {
+    pub section_ids: Vec<u8>,
+    pub sections: HashMap<u8, &'a str>,
+}
+
+impl<'a> GPPStr<'a> {
+    pub fn extract_from_str(s: &'a str) -> Result<Self, GPPDecodeError> {
+        let (section_ids, sections) = extract_gpp_sections_from_str(s)?;
+
+        let sections = section_ids
+            .iter()
+            .zip(sections)
+            .map(|(&id, s)| (id, s))
+            .collect();
+
+        Ok(Self {
+            section_ids,
+            sections,
+        })
+    }
+}
+
+impl<'a> SectionMapper for GPPStr<'a> {
+    fn section(&self, id: u8) -> Option<&str> {
+        self.sections.get(&id).copied()
+    }
+
+    fn section_ids(&self) -> &[u8] {
+        &self.section_ids
+    }
+}
+
+pub struct GPPString {
+    pub section_ids: Vec<u8>,
+    pub sections: HashMap<u8, String>,
+}
+
+impl GPPString {
+    pub fn as_gpp_str(&self) -> GPPStr {
+        GPPStr {
+            section_ids: self.section_ids.clone(),
+            sections: self
+                .sections
+                .iter()
+                .map(|(&id, s)| (id, s.as_str()))
+                .collect(),
+        }
+    }
+}
+
+impl FromStr for GPPString {
+    type Err = GPPDecodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (section_ids, sections) = extract_gpp_sections_from_str(s)?;
+
+        let sections = section_ids
+            .iter()
+            .zip(sections)
+            .map(|(&id, s)| (id, s.to_string()))
+            .collect();
+
+        Ok(Self {
+            section_ids,
+            sections,
+        })
+    }
+}
+
+impl SectionMapper for GPPString {
+    fn section(&self, id: u8) -> Option<&str> {
+        self.sections.get(&id).map(|s| s.as_str())
+    }
+
+    fn section_ids(&self) -> &[u8] {
+        &self.section_ids
+    }
+}
+
+fn extract_gpp_sections_from_str(s: &str) -> Result<(Vec<u8>, Vec<&str>), GPPDecodeError> {
+    let mut sections_iter = s.split('~');
+
+    let header_str = sections_iter.next().ok_or(GPPDecodeError::NoHeaderFound)?;
+    let header = header_str.decode_base64_url()?;
+    let mut reader = DataReader::new(&header);
+
+    let header_type = reader.read_fixed_integer::<u8>(6)?;
+    if header_type != GPP_HEADER {
+        return Err(GPPDecodeError::InvalidHeaderType { found: header_type });
+    }
+
+    let gpp_version = reader.read_fixed_integer::<u8>(6)?;
+    if gpp_version != GPP_VERSION {
+        return Err(GPPDecodeError::InvalidGPPVersion { found: gpp_version });
+    }
+
+    let section_ids = reader.read_fibonacci_range()?;
+    let sections = sections_iter.collect::<Vec<_>>();
+    if sections.len() != section_ids.len() {
+        return Err(GPPDecodeError::IdSectionMismatch {
+            ids: section_ids.len(),
+            sections: sections.len(),
+        });
+    }
+
+    Ok((section_ids, sections))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::sections::id::{TCF_CA, TCF_EU_V2, USP_V1};
 
     #[test]
-    fn parse_str() {
+    fn gpp_model_parse_str() {
         let r = GPPModel::from_str("DBABMA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA").unwrap();
 
         assert_eq!(r.section_ids, vec![TCF_EU_V2]);
@@ -86,7 +199,7 @@ mod tests {
     }
 
     #[test]
-    fn parse_str_multiple_sections() {
+    fn gpp_model_parse_str_multiple_sections() {
         let r = GPPModel::from_str("DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN")
             .unwrap();
 
@@ -96,7 +209,7 @@ mod tests {
     }
 
     #[test]
-    fn parse_str_multiple_sections_unsupported() {
+    fn gpp_model_parse_str_multiple_sections_unsupported() {
         let r =
             GPPModel::from_str("DBABjw~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN").unwrap();
 
@@ -106,5 +219,127 @@ mod tests {
             Section::Unsupported(x) if x == "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
         ));
         assert!(matches!(r.sections[1], Section::UspV1(_)));
+    }
+
+    #[test]
+    fn str_as_gpp_str() {
+        let r = "DBABMA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+            .as_gpp_str()
+            .unwrap();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2]);
+        assert!(matches!(
+            r.section(TCF_EU_V2).unwrap(),
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        ));
+    }
+
+    #[test]
+    fn gpp_str_parse_str() {
+        let r = GPPStr::extract_from_str("DBABMA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA")
+            .unwrap();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2]);
+        assert_eq!(
+            r.sections[&TCF_EU_V2],
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        );
+        assert!(matches!(
+            r.decode_section(TCF_EU_V2),
+            Some(Ok(Section::TcfEuV2(_)))
+        ));
+    }
+
+    #[test]
+    fn gpp_str_parse_str_multiple_sections() {
+        let r =
+            GPPStr::extract_from_str("DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN")
+                .unwrap();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2, USP_V1]);
+        assert_eq!(
+            r.sections[&TCF_EU_V2],
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        );
+        assert_eq!(r.sections[&USP_V1], "1YNN");
+        assert!(matches!(
+            r.decode_section(TCF_EU_V2),
+            Some(Ok(Section::TcfEuV2(_)))
+        ));
+        assert!(matches!(
+            r.decode_section(USP_V1),
+            Some(Ok(Section::UspV1(_)))
+        ));
+    }
+
+    #[test]
+    fn gpp_str_decode_all() {
+        let r =
+            GPPStr::extract_from_str("DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN")
+                .unwrap();
+
+        let s = r.decode_all_sections().unwrap();
+
+        assert!(matches!(s[0], Section::TcfEuV2(_)));
+        assert!(matches!(s[1], Section::UspV1(_)));
+    }
+
+    #[test]
+    fn gpp_string_parse_str() {
+        let r = GPPString::from_str("DBABMA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA").unwrap();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2]);
+        assert_eq!(
+            r.sections[&TCF_EU_V2].as_str(),
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        );
+        assert!(matches!(
+            r.decode_section(TCF_EU_V2),
+            Some(Ok(Section::TcfEuV2(_)))
+        ));
+    }
+
+    #[test]
+    fn gpp_string_parse_str_multiple_sections() {
+        let r = GPPString::from_str("DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN")
+            .unwrap();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2, USP_V1]);
+        assert_eq!(
+            r.sections[&TCF_EU_V2].as_str(),
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        );
+        assert_eq!(r.sections[&USP_V1].as_str(), "1YNN");
+        assert!(matches!(
+            r.decode_section(TCF_EU_V2),
+            Some(Ok(Section::TcfEuV2(_)))
+        ));
+        assert!(matches!(
+            r.decode_section(USP_V1),
+            Some(Ok(Section::UspV1(_)))
+        ));
+    }
+
+    #[test]
+    fn gpp_string_decode_all() {
+        let r = GPPString::from_str("DBACNYA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA~1YNN")
+            .unwrap();
+
+        let s = r.decode_all_sections().unwrap();
+
+        assert!(matches!(s[0], Section::TcfEuV2(_)));
+        assert!(matches!(s[1], Section::UspV1(_)));
+    }
+
+    #[test]
+    fn gpp_string_as_gpp_str() {
+        let r = GPPString::from_str("DBABMA~CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA").unwrap();
+        let r = r.as_gpp_str();
+
+        assert_eq!(r.section_ids, vec![TCF_EU_V2]);
+        assert!(matches!(
+            r.section(TCF_EU_V2).unwrap(),
+            "CPXxRfAPXxRfAAfKABENB-CgAAAAAAAAAAYgAAAAAAAA"
+        ));
     }
 }
